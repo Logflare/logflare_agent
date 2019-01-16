074aa2f0716ab67c5979af4b47c5fcce2ad59882
chasers <chase@pagemodified.com>
2019-01-15 20:26:34 -0700

    Merge branch 'master' of https://github.com/Logflare/logflare-agent

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule LFAgent.MixProject do
       version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Logflare Agent",
+      source_url: "https://github.com/Logflare/logflare-agent",
+      homepage_url: "https://logflare.app",
+      docs: [
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -25,7 +35,8 @@ defmodule LFAgent.MixProject do
     [
       {:httpoison, "~> 1.4"},
       {:jason, "~> 1.1"},
-      {:distillery, "~> 2.0"}
+      {:distillery, "~> 2.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
